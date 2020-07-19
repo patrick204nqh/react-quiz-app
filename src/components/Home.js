1f788f39ca9ceb470ce9a70c1cdd121335c2c4c9
patrick <patrick204nqh@gmail.com>
2020-07-19 12:38:00 +0700

    styling home component with scss

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,20 +9,26 @@ const Home = () => (
     </Helmet>
     <div id='home'>
       <section>
-        <div>
+        <div style={{ textAlign: 'center' }}>
           <span className='mdi mdi-cube-outline cube'></span>
         </div>
         <h1>Quiz App</h1>
         <div className='play-button-container'>
           <ul>
-            <li>
-              <Link to='/play/instructions/'>Play</Link>
+            <li style={{ listStyle: 'none' }}>
+              <Link to='/play/instructions/' className='play-button'>
+                Play
+              </Link>
             </li>
           </ul>
         </div>
         <div className='auth-container'>
-          <Link to='/login'>Login</Link>
-          <Link to='/register'>Register</Link>
+          <Link to='/login' className='auth-buttons' id='login-button'>
+            Login
+          </Link>
+          <Link to='/register' className='auth-buttons' id='signup-button'>
+            Sign up
+          </Link>
         </div>
       </section>
     </div>
