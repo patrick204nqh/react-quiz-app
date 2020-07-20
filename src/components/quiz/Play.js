40efbb51dfea37c1ae7c9623414935c61d8d26da
patrick <patrick204nqh@gmail.com>
2020-07-20 20:50:27 +0700

    adding fifty fifty lifeline

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -157,6 +157,10 @@ class Play extends React.Component {
     options.forEach(option => {
       option.style.visibility = 'visible';
     })
+
+    this.setState({
+      usedFiftyFifty: false
+    })
   }
 
   handleHints = () => {
@@ -189,6 +193,49 @@ class Play extends React.Component {
     }
   }
 
+  handleFiftyFifty = () => {
+    if (this.state.fiftyFifty > 0 && this.state.usedFiftyFifty === false) {
+      const options = document.querySelectorAll('.option');
+      const randomNumbers = [];
+      let indexOfAnswer;
+
+      options.forEach((option, index) => {
+        if (option.innerHTML.toLowerCase() === this.state.answer.toLowerCase()) {
+          indexOfAnswer = index;
+        }
+      })
+
+      let count = 0;
+      do {
+        const randomNumber = Math.round(Math.random() * 3);
+        if (randomNumber !== indexOfAnswer) {
+          if (randomNumbers.length < 2 && !randomNumbers.includes(randomNumber) && !randomNumbers.includes(indexOfAnswer)) {
+            randomNumbers.push(randomNumber);
+            count++;
+          } else {
+            while (true) {
+              const newRandomNumber = Math.round(Math.random() * 3);
+              if (!randomNumbers.includes(newRandomNumber) && !randomNumbers.includes(indexOfAnswer)) {
+                randomNumbers.push(newRandomNumber);
+                count++;
+                break;
+              }
+            }
+          }
+        }
+      } while (count < 2);
+      options.forEach((option, index) => {
+        if (randomNumbers.includes(index)) {
+          option.style.visibility = 'hidden';
+        }
+      });
+      this.setState(prevState => ({
+        fiftyFifty: prevState.fiftyFifty - 1,
+        usedFiftyFifty: true
+      }))
+    }
+  }
+
   render() {
     const { currentQuestion, currentQuestionIndex, numberOfQuestions, hints, fiftyFifty } = this.state;
     return (
@@ -203,7 +250,7 @@ class Play extends React.Component {
           <h2>Quiz Mode</h2>
           <div className="lifeline-container">
             <p>
-              <span className="mdi mdi-set-center mdi-24px lifeline-icon"></span>{' '}
+              <span onClick={this.handleFiftyFifty} className="mdi mdi-set-center mdi-24px lifeline-icon"></span>{' '}
               <span className="lifeline">{fiftyFifty}</span>
             </p>
             <p>
